8d107c0ce1ea54df689c8829bc9e24259f4b1e11
taschekuller <idogukanince@gmail.com>
2025-08-20 11:08:25 +0300

    7 :: solution

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,9 @@
-type MyReadonly<T> = any
+// P 'element1' | 'element2'
+  // element1:???
+  // element2:???
+    //T[P]
+    //readonly element1:string
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]:T[P];
+}
